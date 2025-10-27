8684d225630facfb271684199bdd1e49f9178958
IDontKnow64 <tomtompikachu@gmail.com>
2025-10-27 03:51:04 -0400

    fixed pvp_match.py

--- user/pvp_match.py
+++ user/pvp_match.py
@@ -1,6 +1,6 @@
 from environment.environment import RenderMode, CameraResolution
 from environment.agent import run_real_time_match
-from user.train_agent import UserInputAgent, BasedAgent, GoatedAgent, ConstantAgent, ClockworkAgent, SB3Agent, RecurrentPPOAgent #add anymore custom Agents (from train_agent.py) here as needed
+from training_module.train_agent import UserInputAgent, BasedAgent, GoatedAgent, ConstantAgent, ClockworkAgent, SB3Agent, RecurrentPPOAgent #add anymore custom Agents (from train_agent.py) here as needed
 from user.my_agent import SubmittedAgent
 import pygame
 pygame.init()
