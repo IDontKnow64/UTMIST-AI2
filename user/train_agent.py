c313ba1a8578e7b0d4bd18d475fbfd40ffc7f765
IDontKnow64 <tomtompikachu@gmail.com>
2025-11-01 23:09:44 -0400

    fetch upstream

--- user/train_agent.py
+++ user/train_agent.py
@@ -800,24 +800,24 @@ def gen_reward_manager():
     reward_functions = {
         #'target_height_reward': RewTerm(func=base_height_l2, weight=0.0, params={'target_height': -4, 'obj_name': 'player'}),
         'danger_zone_reward': RewTerm(func=danger_zone_reward, weight=0.5),
-        'damage_interaction_reward': RewTerm(func=damage_interaction_reward, weight=5.0),
+        'damage_interaction_reward': RewTerm(func=damage_interaction_reward, weight=2.0),
         #'head_to_middle_reward': RewTerm(func=head_to_middle_reward, weight=0.01),
         'head_to_opponent': RewTerm(func=head_to_opponent, weight=0.5),
         #'penalize_attack_reward': RewTerm(func=in_state_reward, weight=-0.04, params={'desired_state': AttackState}),
-        'holding_weapon': RewTerm(func=holding_weapon, weight=0.001),
+        'holding_weapon': RewTerm(func=holding_weapon, weight=0.01),
         'holding_more_than_3_keys': RewTerm(func=holding_more_than_3_keys, weight=-0.03),
-        'above_platform': RewTerm(func=above_platform, weight=-0.0005),
+        'above_platform': RewTerm(func=above_platform, weight=0.05),
         #'taunt_reward': RewTerm(func=in_state_reward, weight=0.2, params={'desired_state': TauntState}),
         'has_remaining_jump': RewTerm(func=has_remaining_jump, weight=0.001),
         'has_remaining_recovery': RewTerm(func=has_remaining_recovery, weight=0.001),
         'stay_away_from_edge': RewTerm(func=stay_away_from_edge, weight=0.5),
     }
     signal_subscriptions = {
-        'on_win_reward': ('win_signal', RewTerm(func=on_win_reward, weight=100)),
-        'on_knockout_reward': ('knockout_signal', RewTerm(func=on_knockout_reward, weight=50)),
-        'on_combo_reward': ('hit_during_stun', RewTerm(func=on_combo_reward, weight=20)),
+        'on_win_reward': ('win_signal', RewTerm(func=on_win_reward, weight=20)),
+        'on_knockout_reward': ('knockout_signal', RewTerm(func=on_knockout_reward, weight=8)),
+        'on_combo_reward': ('hit_during_stun', RewTerm(func=on_combo_reward, weight=5)),
         'on_equip_reward': ('weapon_equip_signal', RewTerm(func=on_equip_reward, weight=1)),
-        'on_drop_reward': ('weapon_drop_signal', RewTerm(func=on_drop_reward, weight=10))
+        'on_drop_reward': ('weapon_drop_signal', RewTerm(func=on_drop_reward, weight=2))
     }
     return RewardManager(reward_functions, signal_subscriptions)
 
@@ -875,6 +875,6 @@ if __name__ == '__main__':
         save_handler,
         opponent_cfg,
         CameraResolution.LOW,
-        train_timesteps=2_000_000,
+        train_timesteps=1_000_000,
         train_logging=TrainLogging.PLOT
     )
